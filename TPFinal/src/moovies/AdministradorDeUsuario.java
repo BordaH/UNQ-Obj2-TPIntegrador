85eb02f456b24b7cee1d08e3bc3568f4b36bd1f2
henryborda97 <henryborda17@gmail.com>
2017-05-09 15:43:05 -0300

    Update AdministradorDeUsuario.java

--- TPFinal/src/moovies/AdministradorDeUsuario.java
+++ TPFinal/src/moovies/AdministradorDeUsuario.java
@@ -5,29 +5,35 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+
 public class AdministradorDeUsuario {
 	
 	private ArrayList<Usuario> usuarios =new ArrayList<Usuario>();
 
 	public AdministradorDeUsuario() {
-		usuarios.add(new Usuario("null", "null", 0,"null","null", 0));
+		usuarios.add(new Usuario("No se encontro al usuario"
+				+ "", "null", 0,"null","null", 0));
 	}
 
-	public void registrarUsuario(String nombreU, String apellidoU, int edadU, String generoU, String ocupacionU, int cpU) {
+	public void registrarUsuario(String nombreU, String apellidoU, int edadU, String geneoU, String ocupacionU, int cpU) {
 		usuarios.add(new Usuario( nombreU, apellidoU,edadU, generoU, ocupacionU, cpU));
 	}
 
-	public int cantidadDeUsuarios() {
+	public Integer cantidadDeUsuarios() {
 		return usuarios.size();
 	}
 
-	public Usuario buscarUsuario(String nombreU){
+	public Usuario buscarUsuario(String nombreB){
 	Iterator<Usuario> buscado = usuarios.iterator();
 	int posicionBuscada = 0;
 		while(buscado.hasNext() && posicionBuscada ==0 ){
 			Usuario usuarioB = buscado.next();
 			int idBuscado = 0 ;
-			if (usuarioB.darNombre() != nombreU){
+			if (usuarioB.darNombre() != nombreB){
 				idBuscado = idBuscado + 1 ;
 			}else{
 				
