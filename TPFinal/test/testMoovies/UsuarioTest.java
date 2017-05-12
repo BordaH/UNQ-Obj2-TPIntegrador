520c40792a516abef2a071de5189b727c94a444b
henryborda97 <henryborda17@gmail.com>
2017-05-12 18:08:55 -0300

    Se testea todo en un solo test
    Como verificar la cantidad de calificaciones

--- TPFinal/test/testMoovies/UsuarioTest.java
+++ TPFinal/test/testMoovies/UsuarioTest.java
@@ -7,20 +7,16 @@ import org.junit.Test;
 import moovies.Usuario;
 
 public class UsuarioTest {
-	Usuario usuario = new Usuario(10, 20, "Masculino" , "Gerente", 1884, "Juan", "Perez", 0);
+	Usuario usuario = new Usuario("Juan", "Perez", 20, "Masculino", "Programador", 12);
+
 	@Test
-	public void unUsuarioRegistradoTieneUnId() {
-		assertEquals(usuario.getId(), 10);
-	}
-	@Test
-	public void unUsuarioQueNoRealizoCalificacionesTieneCeroCantidadDeCalificaciones(){
-		assertEquals(usuario.getCantidadDeCalificaciones(),0);
-	}
-	@Test
-	public void unUsuarioQueTieneCeroCantidadDeCalificacionesContabilizadasRealizaUnaCalificacionYAhoraTieneUnaCalificacionPorContar(){
-		assertEquals(usuario.getCantidadDeCalificaciones(),0);
-		usuario.contabilizarCalificacion();
-		assertEquals(usuario.getCantidadDeCalificaciones(),1);
+	public void testDadoUnUsuarioSeVerificaQueConozcaSuNombreApellidoEdadSexoOcupacionYCodPostal() {
+		assertEquals(usuario.darNombre(), "Juan");
+		assertEquals(usuario.darApellido(), "Perez");
+		assertEquals(usuario.darEdad(),20);
+		assertEquals(usuario.genero(), "Masculino");
+		assertEquals(usuario.ocupacion(), "Programador");
+		assertEquals(usuario.codPostal(), 12);
 	}
 
 }
